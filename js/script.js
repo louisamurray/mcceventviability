e588203ff7ba30a1d81916a75c05975ffdb38ead
louisamurray <121989665+louisamurray@users.noreply.github.com>
2023-02-13 20:57:09 +1300

    minor updates

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ function calculate() {
 
   let totalCost = facilitatorCost + venueCost + marketingCost + cateringCost + eventManagerCost + miscCost;
   let ticketPrice = (totalCost / ticketsAvailable) / (1 + (profitMargin / 100));
+  let ticketPriceInclGST = ticketPrice * 1.15;
 
-  document.querySelector('#result').innerHTML = `Ticket price (ex GST): $${ticketPrice.toFixed(2)}`;
+  document.querySelector('#result').innerHTML = `Ticket price (ex GST): $${ticketPrice.toFixed(2)}<br>Ticket price (incl GST): $${ticketPriceInclGST.toFixed(2)}`;
 }
