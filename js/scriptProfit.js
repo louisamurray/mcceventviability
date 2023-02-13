f92bd01e89dfc2a86c963dcd111b4dce2d08aa3d
louisamurray <121989665+louisamurray@users.noreply.github.com>
2023-02-14 12:37:40 +1300

    minor edits

--- js/scriptProfit.js
+++ js/scriptProfit.js
@@ -1,8 +1,8 @@
 function calculate() {
-    let ticketsAvailable = parseInt(document.querySelector('#ticketsAvailable').value);
-    let ticketsPrice = parseInt(document.querySelector('#ticketsPrice').value);
-    let facilitatorCost = parseInt(document.querySelector('#facilitatorCost').value);
-    let venueCost = parseInt(document.querySelector('#venueCost').value);
+    let ticketsAvailable = parseInt(document.querySelector('#ticketsAvailable').value) || 0;
+    let ticketsPrice = parseInt(document.querySelector('#ticketsPrice').value) || 0;
+    let facilitatorCost = parseInt(document.querySelector('#facilitatorCost').value) || 0;
+    let venueCost = parseInt(document.querySelector('#venueCost').value) || 0;
     let marketingCost = 0;
     if (document.querySelector('#radioCampaign').checked) {
         marketingCost += 500;
@@ -13,22 +13,17 @@ function calculate() {
     if (document.querySelector('#socialMediaCampaign').checked) {
         marketingCost += 100;
     }
-    let cateringCost = parseInt(document.querySelector('#cateringCost').value);
-    let eventManagerTime = parseInt(document.querySelector('#eventManagerTime').value);
+    let cateringCost = parseInt(document.querySelector('#cateringCost').value) || 0;
+    let eventManagerTime = parseInt(document.querySelector('#eventManagerTime').value) || 0;
     let eventManagerCost = eventManagerTime * 35;
-    let miscCost = parseInt(document.querySelector('#miscCost').value);
-    let sponsorAmount = parseInt(document.querySelector('#sponsorAmount').value);
+    let miscCost = parseInt(document.querySelector('#miscCost').value) || 0;
+    let sponsorAmount = parseInt(document.querySelector('#sponsorAmount').value) || 0;
     
     let totalCost = facilitatorCost + venueCost + marketingCost + cateringCost + eventManagerCost + miscCost;
     let maxSales = ticketsAvailable * ticketsPrice;
     let totalRevenue = maxSales + sponsorAmount;
     let netProfit = totalRevenue - totalCost;
     let profitMargin = (netProfit / totalRevenue) * 100;
-    
 
-    
-    document.querySelector('#totalRevenue').innerHTML = "$" + totalRevenue.toFixed(2);
-    document.querySelector('#totalCost').innerHTML = "$" + totalCost.toFixed(2);
-    document.querySelector('#netProfit').innerHTML = "$" + netProfit.toFixed(2);
-    document.querySelector('#profitMargin').innerHTML = profitMargin.toFixed(2) + "%";
+    document.querySelector('#result').innerHTML = `Total Revenue (ex GST): $${totalRevenue.toFixed(2)}<br>Total Cost (ex GST): $${totalCost.toFixed(2)}<br>Net Profit (ex GST): $${netProfit.toFixed(2)}<br>Profit Margin: ${profitMargin.toFixed(2)}%`;
 }
--- js/scriptTicket.js
+++ js/scriptTicket.js
@@ -1,7 +1,7 @@
 function calculate() {
-  let ticketsAvailable = parseInt(document.querySelector('#ticketsAvailable').value);
-  let facilitatorCost = parseFloat(document.querySelector('#facilitatorCost').value);
-  let venueCost = parseFloat(document.querySelector('#venueCost').value);
+  let ticketsAvailable = parseInt(document.querySelector('#ticketsAvailable').value) || 0;
+  let facilitatorCost = parseFloat(document.querySelector('#facilitatorCost').value) || 0;
+  let venueCost = parseFloat(document.querySelector('#venueCost').value) || 0;
   let marketingCost = 0;
   if (document.querySelector('#radioCampaign').checked) {
     marketingCost += 500;
@@ -12,12 +12,12 @@ function calculate() {
   if (document.querySelector('#socialMediaCampaign').checked) {
     marketingCost += 100;
   }
-  let cateringCost = parseFloat(document.querySelector('#cateringCost').value);
-  let eventManagerTime = parseFloat(document.querySelector('#eventManagerTime').value);
+  let cateringCost = parseFloat(document.querySelector('#cateringCost').value) || 0;
+  let eventManagerTime = parseFloat(document.querySelector('#eventManagerTime').value) || 0;
   let eventManagerCost = eventManagerTime * 35;
-  let miscCost = parseFloat(document.querySelector('#miscCost').value);
-  let sponsorAmount = parseFloat(document.querySelector('#sponsorAmount').value);
-  let profitMargin = parseFloat(document.querySelector('#profitMargin').value);
+  let miscCost = parseFloat(document.querySelector('#miscCost').value) || 0;
+  let sponsorAmount = parseFloat(document.querySelector('#sponsorAmount').value) || 0;
+  let profitMargin = parseFloat(document.querySelector('#profitMargin').value) || 0;
 
   let totalCost = facilitatorCost + venueCost + marketingCost + cateringCost + eventManagerCost + miscCost;
   let netCost = totalCost - sponsorAmount;
