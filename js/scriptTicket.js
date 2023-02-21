03b045903b42c180d3c11ee6cd09e9d34a8f6ad7
louisamurray <121989665+louisamurray@users.noreply.github.com>
2023-02-22 11:54:48 +1300

    add ability to download results as csv

--- js/scriptTicket.js
+++ js/scriptTicket.js
@@ -27,3 +27,58 @@ function calculate() {
 
   document.querySelector('#result').innerHTML = `Ticket price (ex GST): $${ticketPrice.toFixed(2)}<br>Ticket price (incl GST): $${ticketPriceInclGST.toFixed(2)}<br>Total profit (excl GST): $${totalProfit.toFixed(2)}`;
 }
+
+
+// Add ability to download as .csv
+
+function downloadCSV() {
+  const ticketsAvailable = document.getElementById('ticketsAvailable').value;
+  const facilitatorCost = document.getElementById('facilitatorCost').value;
+  const venueCost = document.getElementById('venueCost').value;
+  const cateringCost = document.getElementById('cateringCost').value;
+  const miscCost = document.getElementById('miscCost').value;
+  const emailCampaign = document.getElementById('emailCampaign').checked;
+  const radioCampaign = document.getElementById('radioCampaign').checked;
+  const socialMediaCampaign = document.getElementById('socialMediaCampaign').checked;
+  const eventManagerTime = document.getElementById('eventManagerTime').value;
+  const sponsorAmount = document.getElementById('sponsorAmount').value;
+  const profitMargin = document.getElementById('profitMargin').value;
+  const resultString = document.getElementById('result').innerHTML;
+
+  // Split the result string on the <br> tag
+  const resultRows = resultString.split('<br>');
+
+  // Create the data in a format that can be easily converted to a CSV file
+  const data = [
+    ['Tickets Available', ticketsAvailable],
+    ['Facilitator Cost', facilitatorCost],
+    ['Venue Cost', venueCost],
+    ['Catering Cost', cateringCost],
+    ['Misc Cost', miscCost],
+    ['Email Campaign', emailCampaign ? 'Yes' : 'No'],
+    ['Radio Campaign', radioCampaign ? 'Yes' : 'No'],
+    ['Social Media Campaign', socialMediaCampaign ? 'Yes' : 'No'],
+    ['Event Manager Time', eventManagerTime],
+    ['Sponsor Amount', sponsorAmount],
+    ['Desired Profit Margin', profitMargin],
+  ];
+
+  // Add each result row to the data array
+  resultRows.forEach((resultRow) => {
+    const [label, value] = resultRow.split(': ');
+    data.push([label, value]);
+  });
+
+  // Create the CSV string
+  let csvContent = 'data:text/csv;charset=utf-8,';
+  data.forEach((row) => {
+    csvContent += row.join(',') + '\r\n';
+  });
+
+  // Encode the CSV string as a URI and create a download link
+  const encodedUri = encodeURI(csvContent);
+  const link = document.createElement('a');
+  link.setAttribute('href', encodedUri);
+  link.setAttribute('download', 'ticket-data.csv');
+  link.click();
+}
